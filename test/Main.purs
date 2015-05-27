53b64ff36cfedd7b8058ab826c9987ad6b747d5e
holoed@users.noreply.github.com <edmondo@gmail.com>
2015-05-27 13:21:51 +0100

    Add some tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,17 @@
 module Test.Main where
 
-import Debug.Trace
+import Test.Unit
+import SearchEngine
 
-main = do
-  trace "You should add some tests."
+main = runTest do
+
+  test "Empty Index Search tests" do
+    let index = createIndex []
+    assert "Empty index with empty string" $ search index "" == []
+    assert "Empty index with non-empty string" $ search index "Foo" == []
+
+  test "One word Index Search tests" $ do
+    let index = createIndex ["Hello"]
+    assert "Search with empty string" $ search index "" == []
+    assert "Search with non existing word" $ search index "Foo" == []
+    assert "Search with existing word" $ search index "Hello" == [1]
